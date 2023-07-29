c0c3bb4dc1f70e467d260e14d0a63d4b9502c7ad
Rohit Tewari <rtewari056@gmail.com>
2023-07-29 20:59:06 +0530

    Custom Observable in Rxjs part 2

--- src/app/components/custom-observable/custom-observable.component.ts
+++ src/app/components/custom-observable/custom-observable.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Observable, Subscriber } from 'rxjs';
+import { Observable, Subject, Subscriber, Subscription, takeUntil } from 'rxjs';
 import { DesignUtilityService } from 'src/app/services/design-utility.service';
 
 @Component({
@@ -10,40 +10,102 @@ import { DesignUtilityService } from 'src/app/services/design-utility.service';
   templateUrl: './custom-observable.component.html',
   styleUrls: ['./custom-observable.component.scss']
 })
-export class CustomObservableComponent implements OnInit {
+export class CustomObservableComponent implements OnInit, OnDestroy {
   private _designUtilityService = inject(DesignUtilityService);
 
-  obsStatus: string = '';
+  obsStatus1: string = '';
+  obsStatus2: string = '';
+  
+  name!: string;
+  nameStatus: string = '';
+
+  unsubscribe$: Subject<void> = new Subject<void>;
 
   ngOnInit(): void {
     // EX - 01 (Manual)
-    const cusObs1$ = new Observable((subscriber: Subscriber<string>) => {
+    const cusObs1$: Observable<string> = new Observable((subscriber: Subscriber<string>) => {
       setTimeout(() => subscriber.next('Angular'), 1000);
       setTimeout(() => subscriber.next('TypeScript'), 2000);
-      setTimeout(() => subscriber.next('HTML & CSS'), 3000);
+      setTimeout(() => {
+        subscriber.next('HTML & CSS');
+        subscriber.complete();
+      }, 3000);
       setTimeout(() =>{ 
         subscriber.next('Javascript');
-        subscriber.error(new Error('Limit exceed'));
+        // subscriber.error(new Error('Limit exceed'));
       }, 4000);
       setTimeout(() => {
         subscriber.next('React');
-        subscriber.complete();
+        // subscriber.complete();
       }, 5000);
     });
     
     // subscribe(data, error, completion)
     cusObs1$.subscribe((res: string) => {
-      console.log(res);
       this._designUtilityService.print(res, 'custObsManualContainer');
     },(error: Error) => {
-      this.obsStatus = 'error';
+      this.obsStatus1 = 'error';
     }, () => {
-      this.obsStatus = 'completed';
+      this.obsStatus1 = 'completed';
+    });
+    
+    // EX - 02 (Custom Interval Observable)
+    const arr2: string[] = ['Angular', 'JavaScript', 'HTML', 'CSS', 'TypeScript'];
+    
+    const cusObs2$: Observable<string> = new Observable((subscriber: Subscriber<string>) => {
+      let count: number = 0;
+      
+      setInterval(() => {
+        subscriber.next(arr2[count]);
+        
+        if(count >= 2) subscriber.error('Error emit');
+        
+        if(count >= 5) subscriber.complete();
+        
+        count++;
+      }, 1000);
+    })
+    
+    cusObs2$.pipe(takeUntil(this.unsubscribe$)).subscribe((res: string) => {
+      this._designUtilityService.print(res, 'custInterObsContainer');
+      // console.log(res);
+    },(error: Error) => {
+      this.obsStatus2 = 'error';
+    }, () => {
+      this.obsStatus2 = 'completed';
     });
 
-    // EX - 02 (Custom Interval Observable)
-
     // EX - 03 (Random Names)
+    const arr3: string[] = ['Rohit', 'Aman', 'Chaman', 'Sameer', 'John', 'Alex', 'Robert'];
+
+    const cusObs3$ = new Observable((subscriber: Subscriber<string>) => {
+      let count: number = 0;
+      
+      setInterval(() => {
+        subscriber.next(arr3[count]);
+        
+        // if(count >= 4) subscriber.error('Error emit');
+        
+        if(count >= 6) subscriber.complete();
+        
+        count++;
+      }, 1000);
+
+    });
+
+    cusObs3$.pipe(takeUntil(this.unsubscribe$)).subscribe((res: string) => {
+      this.name = res;
+      // console.log(res);
+    },(error: Error) => {
+      this.nameStatus = 'error';
+    }, () => {
+      this.nameStatus = 'completed';
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next()
+    this.unsubscribe$.complete()
   }
 
 }
